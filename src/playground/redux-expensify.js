b2390ceaa9a053582f90207b64dec563616c816c
Lex Blagus <lex@blag.us>
2017-10-10 23:58:02 -0300

    Redux, reducers, store: CR_D

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,6 @@
 import { createStore, combineReducers } from 'redux';
 import uuid from 'uuid';
+var console = require("console");
 
 
 
@@ -20,17 +21,29 @@ const addExpense = ({
 	}
 });
 
+const removeExpense = ({
+	id = ''
+} = {}) => ({
+	type: 'REMOVE_EXPENSE',
+	id
+});
 
+// remove expense
 
 const expensesReducerDefaultState = [];
-const expensesReducer = (state = expensesReducerDefaultState, action) => {
+const expensesReducer = (
+	state = expensesReducerDefaultState,
+	action
+) => {
 	switch (action.type){
 		case 'ADD_EXPENSE':
 			//return state.concat(action.expense)
 			return [
-				action.expense,
-				...state
+				...state,
+				action.expense
 			];
+		case 'REMOVE_EXPENSE':
+			return  state.filter(({ id }) => id!==action.id);
 		default:
 			return state;
 	}
@@ -58,19 +71,33 @@ const store = createStore(
 		filters: filtersReducer
 	})
 );
-console.log( store.getState() );
+console.log('initial store.getState() = ',  JSON.stringify(store.getState(), null, '	') );
 
 store.subscribe(() => {
-	console.log(store.getState())
+	console.log('subscribed store.getState() = ', JSON.stringify(store.getState(), null, '	'))
 });
 
-store.dispatch(
+
+store.dispatch(  addExpense({ description: 'begin', note:'BOF', amount: 1 })  );
+
+const expenseOne = store.dispatch(
 	addExpense({ description: 'rent', note:'YES', amount: 100 })
 );
+console.log('expenseOne.expense.id = ', expenseOne.expense.id);
 
-store.dispatch(
+const expenseTwo = store.dispatch(
 	addExpense({ description: 'cofee', note:'NO', amount: 200 })
 );
+console.log('expenseTwo.expense.id = ', expenseTwo.expense.id);
+
+store.dispatch(  addExpense({ description: 'end', note:'EOF', amount: 10 })  );
+
+
+console.warn(JSON.stringify(store.getState(), null, '	'));
+const expenseThree = store.dispatch(
+	removeExpense({ id: expenseOne.expense.id })
+);
+console.warn(JSON.stringify(store.getState(), null, '	'));
 
 
 const demoState = {
