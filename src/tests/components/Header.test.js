b13ab1aeb0b8b895322e0758c52928987751dc0d
Lex Blagus <lex@blag.us>
2017-11-20 18:58:15 -0200

    119. Enzyme

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+// import ReactShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
 import Header from '../../components/Header.js';
 
 test('should render Header',()=>{
-	const renderer = new ReactShallowRenderer();
-	renderer.render(<Header />);
-	expect(renderer.getRenderOutput()).toMatchSnapshot();
+	//const renderer = new ReactShallowRenderer();
+	//renderer.render(<Header />);
+	//expect(renderer.getRenderOutput()).toMatchSnapshot();
+	
+	const wrapper = shallow(
+		<Header />
+	);
+	
+	//expect(wrapper.find('NavLink').length).toBe(5);
+	expect( wrapper ).toMatchSnapshot();
 });
