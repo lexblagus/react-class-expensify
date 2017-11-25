ac87096e258f9ec0da455780cb6bda8609206c0a
Lex Blagus <lex@blag.us>
2017-11-25 03:16:44 -0200

    Setup prod build and server for Heroku

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const pubPath = path.join(__dirname, '..', 'public');
+app.use(express.static(pubPath));
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(pubPath, 'index.html'));
+});
+
+app.listen(port, ()=>{
+	console.log('Server up');
+});
