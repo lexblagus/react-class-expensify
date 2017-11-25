8eaf649a13775b1a63b2c6a899d77318e8bcf87d
Lex Blagus <lex@blag.us>
2017-11-25 02:43:45 -0200

    Setup prod webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,69 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-	entry: './src/app.js',
-	//entry: './src/playground/destructuring.js',
-	//entry: './src/playground/redux-101.js',
-	//entry: './src/playground/redux-expensify.js',
-	//entry: './src/playground/hoc.js',
-	output: {
-		path: path.join(__dirname, 'public'),
-		filename: 'bundle.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/
-			}, {
-				test: /\.(css|sass|scss)/,
-				use: [
-					'style-loader',
-					'css-loader',
-					'sass-loader'
-				]
-			}
+module.exports = (env) => {
+
+	console.log('');
+	console.log('env', env);
+	console.log('');
+
+	const isProd = env==='production';
+	const CSSExtract = new ExtractTextPlugin('styles.css');
+
+	return {
+		entry: './src/app.js',
+		//entry: './src/playground/destructuring.js',
+		//entry: './src/playground/redux-101.js',
+		//entry: './src/playground/redux-expensify.js',
+		//entry: './src/playground/hoc.js',
+		output: {
+			path: path.join(__dirname, 'public'),
+			filename: 'bundle.js'
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					loader: 'babel-loader',
+					exclude: /node_modules/
+				}, {
+					test: /\.(css|sass|scss)/,
+					use: CSSExtract.extract({
+						use: [
+							{
+								loader: 'css-loader',
+								options: {
+									sourceMap: true
+								}
+							},{
+								loader: 'sass-loader',
+								options: {
+									sourceMap: true
+								}
+							}
+						]
+					})
+					/*
+					[
+						'style-loader',
+						'css-loader',
+						'sass-loader'
+					]
+					*/
+				}
+			],
+			loaders : [
+				
+			]
+		},
+		plugins: [
+			CSSExtract
 		],
-		loaders : [
-			
-		]
-	},
-	devtool: 'source-map',
-	devServer: {
-		contentBase: path.join(__dirname, 'public'),
-		historyApiFallback: true,
-		clientLogLevel: "info"
+		devtool: isProd ? 'source-map' : 'inline-source-map',
+		devServer: {
+			contentBase: path.join(__dirname, 'public'),
+			historyApiFallback: true,
+			clientLogLevel: "info"
+		}
 	}
 };
